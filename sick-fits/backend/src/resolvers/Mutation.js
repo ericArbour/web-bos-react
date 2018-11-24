ad202b77acaabdb94d589e0379605b1a77213c85
Eric Arbour <arboureric@gmail.com>
2018-11-24 16:02:25 -0500

    finish video 41

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -199,6 +199,44 @@ const Mutations = {
       },
       info
     );
+  },
+  async addToCart(parent, args, ctx, info) {
+    // make sure they are signed in
+    const { userId } = ctx.request;
+    if (!userId) throw new Error("You must be signed in");
+    // query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    });
+    // check if that item is already in their cart and increment by one if it is
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+    // if its not already in their cart, create a fresh CartItem for that user
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId }
+          },
+          item: {
+            connect: {
+              id: args.id
+            }
+          }
+        }
+      },
+      info
+    );
   }
 };
 
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import formatMoney from "../lib/formatMoney";
 
+import AddToCart from "./AddToCart";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
@@ -30,7 +31,7 @@ class Item extends Component {
           <Link href={{ pathname: "/update", query: { id: item.id } }}>
             <a>Edit</a>
           </Link>
-          <button>Add To Cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
